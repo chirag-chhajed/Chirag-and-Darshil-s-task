b760a401d1baf9577504fc86558c9e6b615cbe14
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-08-19 16:52:23 +0530

    change made in js

--- COLOR FLIPPER/JS/app.js
+++ COLOR FLIPPER/JS/app.js
@@ -1,14 +1,21 @@
+// Selecting Elements
+
 const button = document.getElementById("btn")
 const color = document.querySelector(".color")
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-function getRandomNumber(){
-    return Math.floor(Math.random()*hex.length)
+// Random function
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
 }
 
-btn.addEventListener('click', function(){
+// Event Listener
+
+
+btn.addEventListener('click', function () {
     let hexColor = "#";
-    for(let i = 0; i<6; i++){
+    for (let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()];
     }
     color.textContent = hexColor;
