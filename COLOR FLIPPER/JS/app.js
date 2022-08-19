649e542251b70f8f588789672819de4245d659cb
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-08-19 16:21:30 +0530

    change made

---
+++ COLOR FLIPPER/JS/app.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("btn")
+const color = document.querySelector(".color")
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*hex.length)
+}
+
+btn.addEventListener('click', function(){
+    let hexColor = "#";
+    for(let i = 0; i<6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+    document.body.style.transition = "all ease-in-out 400ms"
+    button.style.backgroundColor = hexColor;
+
+})
